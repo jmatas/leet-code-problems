ec6f89495db1b8b86aec725cf5b4566b5576a353
jmatas <joan.matas@gmail.com>
2023-04-29 23:16:22 +0200

    feat: Add another solution looking for the best performance

---
+++ src/main/java/org/jmatas/problems/addtwonumbers/AddTwoNumbersPerfomanceBasedSolution.java
@@ -0,0 +1,70 @@
+package org.jmatas.problems.addtwonumbers;
+
+public class AddTwoNumbersPerfomanceBasedSolution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode firstNumber = null;
+        ListNode previousNumber = null;
+
+        ListNode current1 = l1;
+        ListNode current2 = l2;
+        int remaining = 0;
+        do {
+
+            int value1 = getValue(current1);
+            int value2 = getValue(current2);
+
+            int currentValue = value1 + value2 + remaining;
+            int value = currentValue % 10;
+            remaining = currentValue/10;
+
+            if (firstNumber == null) {
+                firstNumber = new ListNode(value);
+                previousNumber = firstNumber;
+            } else {
+                previousNumber.next = new ListNode(value);
+                previousNumber = previousNumber.next;
+            }
+
+
+            if (current1 != null) {
+                current1 = current1.next;
+            }
+            if (current2  != null) {
+                current2 = current2.next;
+            }
+
+        } while (current1 != null || current2 != null || remaining > 0);
+
+        return firstNumber;
+    }
+
+    private static int getValue(ListNode current1) {
+        int value1 = 0;
+        if (current1 != null) {
+            value1 = current1.val;
+        }
+        return value1;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
---
+++ src/test/java/org/jmatas/problems/addtwonumbers/AddTwoNumbersPerformaceBasedSolutionTest.java
@@ -0,0 +1,73 @@
+package org.jmatas.problems.addtwonumbers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
+
+import org.jmatas.problems.addtwonumbers.AddTwoNumbersPerfomanceBasedSolution.ListNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class AddTwoNumbersPerformaceBasedSolutionTest {
+
+    private AddTwoNumbersPerfomanceBasedSolution solution;
+
+    @BeforeEach
+    public void setUp() {
+        solution = new AddTwoNumbersPerfomanceBasedSolution();
+    }
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    public void given_test_cases(String testCaseName, ListNode summand1, ListNode summand2, ListNode expectedResult) {
+        // Arrange
+
+        // Action
+        ListNode result = solution.addTwoNumbers(summand1, summand2);
+
+        // Assert
+        ListNode resultCurrent = result;
+        ListNode expectedResultCurrent = expectedResult;
+        do {
+            assertThat(resultCurrent.val).isEqualTo(expectedResultCurrent.val);
+            resultCurrent = resultCurrent.next;
+            expectedResultCurrent = expectedResultCurrent.next;
+        } while (expectedResultCurrent != null);
+    }
+
+    private static Stream<Arguments> testCases() {
+        new ListNode(2, new ListNode(4, new ListNode(3)));
+        return Stream.of(
+
+                arguments("Example 1",
+                        new ListNode(2, new ListNode(4, new ListNode(3))),
+                        new ListNode(5, new ListNode(6, new ListNode(4))),
+                        new ListNode(7, new ListNode(0, new ListNode(8)))),
+                arguments("Example 2",
+                        new ListNode(0),
+                        new ListNode(0),
+                        new ListNode(0)),
+                arguments("Example 3",
+                        new ListNode(9,
+                                new ListNode(9,
+                                        new ListNode(9,
+                                                new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))),
+                        new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))),
+                        new ListNode(8,
+                                new ListNode(9, new ListNode(9,
+                                        new ListNode(9,
+                                                new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1))))))))),
+                arguments("Example 4",
+                        new ListNode(9),
+                        new ListNode(1, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9,
+                                new ListNode(9,
+                                        new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))))))),
+                        new ListNode(0, new ListNode(0,
+                                new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0,
+                                        new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1))))))))))))
+                                        );
+    }
+}
