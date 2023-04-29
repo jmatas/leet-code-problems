3d3f6ae51d8bca6418ba99c2fa1c1111ae4a348a
jmatas <joan.matas@gmail.com>
2023-04-29 17:51:36 +0200

    chore: Create problem

---
+++ src/main/java/org/jmatas/Main.java
@@ -0,0 +1,16 @@
+package org.jmatas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Solution().twoSum(new int[] {2, 7, 11, 15}, 9);
+    }
+
+    static class Solution {
+
+        public int[] twoSum(int[] nums, int target) {
+            System.out.println("Hello world!");
+            return null;
+        }
+    }
+}
