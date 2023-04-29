21c6448ceffb1d96e61e358e4e70228fc0c23b04
jmatas <joan.matas@gmail.com>
2023-04-29 17:53:42 +0200

    feat: Create first solution

--- src/main/java/org/jmatas/Solution.java
+++ src/main/java/org/jmatas/Solution.java
@@ -1,9 +1,22 @@
 package org.jmatas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
     public int[] twoSum(int[] nums, int target) {
-        System.out.println("Hello world!");
-        return null;
+        Map<Integer, Integer> trackNumbers = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentNumber = nums[i];
+
+            if (trackNumbers.containsKey(target - currentNumber)) {
+                return new int[] {trackNumbers.get(target - currentNumber), i};
+            }
+            trackNumbers.put(currentNumber, i);
+        }
+
+        throw new IllegalArgumentException("No solution found for the given input");
     }
 }
