77cf2ec44f9427bee6b63dd7de29709f491cfd42
jmatas <joan.matas@gmail.com>
2023-04-29 22:34:15 +0200

    feat: Recursive solution with string

--- src/main/java/org/jmatas/addtwonumbers/AddTwoNumbersSolution.java
+++ src/main/java/org/jmatas/addtwonumbers/AddTwoNumbersSolution.java
@@ -1,9 +1,29 @@
 package org.jmatas.addtwonumbers;
 
+import java.math.BigDecimal;
+
 public class AddTwoNumbersSolution {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode(7, new ListNode(0,new ListNode(8)));
+        BigDecimal summand1 = new BigDecimal(toDecimal(l1));
+        BigDecimal summand2 = new BigDecimal(toDecimal(l2));
+        return toListNode(summand1.add(summand2).toString());
+    }
+
+    public ListNode toListNode(String number) {
+        if (number.length() <= 1) {
+            return new ListNode(Integer.parseInt(number));
+        }
+        return new ListNode(
+                Integer.parseInt(number.substring(number.length() - 1)),
+                toListNode(number.substring(0, number.length() - 1)));
+    }
+
+    public String toDecimal(ListNode l1) {
+        if (l1.next == null) {
+            return String.valueOf(l1.val);
+        }
+        return toDecimal(l1.next) + l1.val;
     }
 
     /**
--- src/test/java/org/jmatas/addtwonumbers/AddTwoNumbersSolutionTest.java
+++ src/test/java/org/jmatas/addtwonumbers/AddTwoNumbersSolutionTest.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 
 import org.jmatas.addtwonumbers.AddTwoNumbersSolution.ListNode;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -18,6 +19,19 @@ class AddTwoNumbersSolutionTest {
     @BeforeEach
     public void setUp() {
         solution = new AddTwoNumbersSolution();
+    }
+
+    @Test
+    void transform_from_numeric_to_list_test() {
+        // Arrange
+        String givenNumber = "23423423443436151413431";
+
+        // Action
+        ListNode listNode = solution.toListNode(givenNumber);
+        String result = solution.toDecimal(listNode);
+
+        // Assert
+        assertThat(result).isEqualTo(givenNumber);
     }
 
     @ParameterizedTest
@@ -35,34 +49,38 @@ class AddTwoNumbersSolutionTest {
             assertThat(resultCurrent.val).isEqualTo(expectedResultCurrent.val);
             resultCurrent = resultCurrent.next;
             expectedResultCurrent = expectedResultCurrent.next;
-        } while (expectedResultCurrent!= null);
+        } while (expectedResultCurrent != null);
     }
 
     private static Stream<Arguments> testCases() {
-
-        new ListNode(2, new ListNode(4,new ListNode(3)));
+        new ListNode(2, new ListNode(4, new ListNode(3)));
         return Stream.of(
 
                 arguments("Example 1",
-                        new ListNode(2, new ListNode(4,new ListNode(3))),
-                        new ListNode(5, new ListNode(6,new ListNode(4))),
-                        new ListNode(7, new ListNode(0,new ListNode(8)))
-                ),
+                        new ListNode(2, new ListNode(4, new ListNode(3))),
+                        new ListNode(5, new ListNode(6, new ListNode(4))),
+                        new ListNode(7, new ListNode(0, new ListNode(8)))),
                 arguments("Example 2",
                         new ListNode(0),
                         new ListNode(0),
-                        new ListNode(0)
-                ),
+                        new ListNode(0)),
                 arguments("Example 3",
-                        new ListNode(9, new ListNode(9,new ListNode(9,new ListNode(9, new ListNode(9,new ListNode(9,new ListNode(9))))))),
-                        new ListNode(9, new ListNode(9,new ListNode(9,new ListNode(9)))),
-                        new ListNode(8, new ListNode(9,new ListNode(9,new ListNode(9, new ListNode(0,new ListNode(0,new ListNode(0, new ListNode(1))))))))
-                ),
+                        new ListNode(9,
+                                new ListNode(9,
+                                        new ListNode(9,
+                                                new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))),
+                        new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))),
+                        new ListNode(8,
+                                new ListNode(9, new ListNode(9,
+                                        new ListNode(9,
+                                                new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1))))))))),
                 arguments("Example 4",
                         new ListNode(9),
-                        new ListNode(1, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9,new ListNode(9,new ListNode(9)))))))))),
-                        new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0,new ListNode(0,new ListNode(0, new ListNode(1)))))))))))
-                )
-        );
+                        new ListNode(1, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9,
+                                new ListNode(9,
+                                        new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))))))),
+                        new ListNode(0, new ListNode(0,
+                                new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0,
+                                        new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1)))))))))))));
     }
 }
