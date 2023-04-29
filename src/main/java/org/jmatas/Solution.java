1a5ff7675bef9d5185bd2c19faecf69917481538
jmatas <joan.matas@gmail.com>
2023-04-29 17:52:24 +0200

    test: Adds given tests (failing)

---
+++ src/main/java/org/jmatas/Solution.java
@@ -0,0 +1,9 @@
+package org.jmatas;
+
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        System.out.println("Hello world!");
+        return null;
+    }
+}
---
+++ src/test/java/org/jmatas/SolutionTest.java
@@ -0,0 +1,52 @@
+package org.jmatas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    public void given_test_cases(String testCaseName, int[] nums, int target, int[] expectedResult) {
+        // Arrange
+
+        // Action
+        int[] result = solution.twoSum(nums, target);
+
+        // Assert
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> testCases() {
+        return Stream.of(
+
+                arguments("Example 1",
+                        new int[] {2, 7, 11, 15},
+                        9,
+                        new int[] {0, 1}),
+
+                arguments("Example 2",
+                        new int[] {3, 2, 4},
+                        6,
+                        new int[] {1, 2}),
+
+                arguments("Example 3",
+                        new int[] {3, 3},
+                        6,
+                        new int[] {0, 1}));
+    }
+}
